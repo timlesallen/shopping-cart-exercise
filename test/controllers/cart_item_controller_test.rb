8092c8bc69b12c20f4b57b7e82eab1432bc528f8
Tim Allen <tim@noblesamurai.com>
2020-11-07 20:17:10 +1100

    Simple test for cart item creation.

--- test/controllers/cart_item_controller_test.rb
+++ test/controllers/cart_item_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CartItemControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create cart item" do
+    assert_difference('CartItem.count') do
+      post cart_items_url, params: { uuid: Product.first.uuid }
+    end
+    assert_redirected_to('/')
+  end
 end
