bb58fc8d8f5a0fa8cb962cacaef80736c9026658
Tim Allen <tim@noblesamurai.com>
2020-11-07 20:35:53 +1100

    Test deletion of cart item.

--- test/controllers/cart_item_controller_test.rb
+++ test/controllers/cart_item_controller_test.rb
@@ -16,4 +16,13 @@ class CartItemControllerTest < ActionDispatch::IntegrationTest
     assert_equal(CartItem.find_by(product: Product.first).quantity, 2)
     assert_redirected_to('/')
   end
+
+  test "should completely delete an existing cart item" do
+    post cart_items_url, params: { uuid: Product.first.uuid }
+    cartItem = CartItem.first
+    assert_difference('CartItem.count', -1) do
+      delete cart_item_url(id: cartItem.id)
+    end
+    assert_redirected_to('/')
+  end
 end
