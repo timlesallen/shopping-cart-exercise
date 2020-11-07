b91c450a144d1d9b4096dc15550280d6aac2ee65
Tim Allen <tim@noblesamurai.com>
2020-11-07 17:00:20 +1100

    Correct implementation of total_price.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,8 @@ class Cart < ApplicationRecord
   has_many :cart_items
 
   def total_price
-    0
+    cart_items.reduce(0) do |sum, item|
+      sum + item.product.price
+    end
   end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,4 +4,11 @@ class CartTest < ActiveSupport::TestCase
   test "total_price is zero for empty cart" do
     assert Cart.new(cart_items: []).total_price === 0
   end
+
+  test "total_price is correct for populated cart" do
+    assert Cart.new(cart_items: [
+        CartItem.new(product: Product.new(price: 101.01)),
+        CartItem.new(product: Product.new(price: 2.00))]
+      ).total_price === 103.01
+  end
 end
