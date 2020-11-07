6a1368e04325441bdc4f3e87fe35e07879ecb7fd
Tim Allen <tim@noblesamurai.com>
2020-11-07 20:55:43 +1100

    Implement discount.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,4 +6,21 @@ class Cart < ApplicationRecord
       sum + (item.product.price * item.quantity)
     end
   end
+
+  def discount
+    discount_factor(self.total_price) * self.total_price
+  end
+
+  private
+  def discount_factor total
+    if total > 100
+      0.2
+    elsif total > 50
+      0.15
+    elsif total > 20
+      0.1
+    else
+      0
+    end
+  end
 end
