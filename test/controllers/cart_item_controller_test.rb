0d5e3531484c1b13215d998e63fc2e33078464d4
Tim Allen <tim@noblesamurai.com>
2020-11-07 20:22:39 +1100

    Failing test for quantity incrementation.

--- test/controllers/cart_item_controller_test.rb
+++ test/controllers/cart_item_controller_test.rb
@@ -7,4 +7,12 @@ class CartItemControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to('/')
   end
+  test "should increment existing cart item quantity" do
+    post cart_items_url, params: { uuid: Product.first.uuid }
+    assert_no_difference('CartItem.count') do
+      post cart_items_url, params: { uuid: Product.first.uuid }
+    end
+    assert_equal(CartItem.find_by(product: Product.first).quantity, 2)
+    assert_redirected_to('/')
+  end
 end
