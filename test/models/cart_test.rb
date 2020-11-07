2ae859beee445a06b89cf1b1f62a52d05998acd6
Tim Allen <tim@noblesamurai.com>
2020-11-07 20:48:31 +1100

    Failing tests for discounts.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -8,7 +8,25 @@ class CartTest < ActiveSupport::TestCase
   test "total_price is correct for populated cart" do
     assert Cart.new(cart_items: [
         CartItem.new(product: Product.new(price: 101.01), quantity: 2),
-        CartItem.new(product: Product.new(price: 2.00))]
-      ).total_price === 204.02
+        CartItem.new(product: Product.new(price: 2.00))
+    ]).total_price === 204.02
+  end
+
+  test "discount of 10% applied for > $20" do
+    assert Cart.new(cart_items: [
+        CartItem.new(product: Product.new(price: 30))
+    ]).discount === 3
+  end
+
+  test "discount of 15% applied for > $50" do
+    assert Cart.new(cart_items: [
+        CartItem.new(product: Product.new(price: 60))
+    ]).discount === 9
+  end
+
+  test "discount of 20% applied for > $100" do
+    assert Cart.new(cart_items: [
+        CartItem.new(product: Product.new(price: 120))
+    ]).discount === 24
   end
 end
